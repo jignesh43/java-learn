2b8e469b2f3105f38a539033056e75f5f3591b3d
jignesh@gmail.com <105378421+jignesh43@users.noreply.github.com>
2024-04-05 22:12:56 +0530

    ittration in java

---
+++ Javaittration.java
@@ -0,0 +1,12 @@
+public class Javaittration {
+    public static void main(String []arg){
+
+        //itration loop
+        int[] numbers = {1, 2, 3, 4, 5};
+        for(int i : numbers){
+            System.out.println(i);
+        }
+
+
+    }
+}
