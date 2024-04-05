0de70b62af0f15187cc4b4eaa29e7dc66b6a5185
jignesh@gmail.com <105378421+jignesh43@users.noreply.github.com>
2024-04-05 20:39:35 +0530

    logical operator

---
+++ Logical.java
@@ -0,0 +1,30 @@
+public class Logical {
+    public static void main(String[]arg){
+        //logical operater
+
+        // AND
+        int a=10;
+        int b=2;
+        int c=3;
+        if(a>b && a>c){
+            System.out.println("both condition is true");
+        }
+        else {
+            System.out.println("both condition is not true");
+
+        }
+        //OR
+        int d=10;
+        int e=2;
+        int f=5;
+        if(d>e || d>e){
+            System.out.println("one condition is true ");
+        }
+        else {
+            System.out.println("both condition is not true");
+
+        }
+
+
+    }
+}
