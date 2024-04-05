33d36f3191e4acff3d6b92e82b7c1101e5ab2218
jignesh@gmail.com <105378421+jignesh43@users.noreply.github.com>
2024-04-05 21:45:24 +0530

    loop in java

--- loop.java
+++ loop.java
@@ -1,2 +1,23 @@
 public class loop {
+    public static void main(String []arg){
+
+        //loop in java
+        for (int i =0;i<10;i++){
+            System.out.println(i);
+        }
+        // while loop
+        int k=0;
+        while(k<15){
+            System.out.println(k);
+            k++;
+        }
+        //do-while loop
+        int t=0;
+        do {
+            System.out.println(t);
+            t++;
+        }
+        while (t<15);
+
+    }
 }
